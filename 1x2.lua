3e48f2d30c2c7e49a157c0d14f71291349b06ecf
Koganehikari <93547229+Koganehikari@users.noreply.github.com>
2021-11-02 15:03:11 -0300

    Update 1x2.lua

--- 1x2.lua
+++ 1x2.lua
@@ -1,4 +1,3 @@
-function digForward()
 slotAtual = 2
 
 
@@ -37,30 +36,3 @@ end
 while (true) do
     Forward()
 end
-
-    while (turtle.detect()) do
-        turtle.digDown()
-        turtle.dig()
-    end
-end
-
-function refuel()
-    turtle.select(1)
-    turtle.refuel(1)
-end
-
-function Forward()
-    if turtle.getFuelLevel() < 5 then
-        refuel()
-    end
-    
-    while turtle.forward() == false do
-        turtle.attack()
-        digForward()
-    end 
-end
-
-while (true) do
-    Forward()
-end
-
